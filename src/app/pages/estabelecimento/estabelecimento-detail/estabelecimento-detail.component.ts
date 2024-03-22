3ccef5f2a959eb2ee8b035d9bf4b1f34eeb62179
alessandro <alessandro-cav@hotmail.com>
2024-03-21 22:46:22 -0300

    Criando o primeiro card

--- src/app/pages/estabelecimento/estabelecimento-detail/estabelecimento-detail.component.ts
+++ src/app/pages/estabelecimento/estabelecimento-detail/estabelecimento-detail.component.ts
@@ -13,8 +13,17 @@ export class EstabelecimentoDetailComponent {
   isEditing: boolean = false;
 
   form = new FormGroup({
-    nomeFantasia:new FormControl('',[Validators.required, Validators.email])
+    nomeFantasia:new FormControl([Validators.required]),
+    razaoSocial:new FormControl([Validators.required]),
+    cnpj:new FormControl([Validators.required]),
+    email:new FormControl('',[Validators.required, Validators.email]),
+    telefone:new FormControl('',[Validators.required]),
+    categoriaNegocio:new FormControl('',[Validators.required]),
+    dataCriacao:new FormControl('',[Validators.required]),
+    nomeComprovante:new FormControl('',[Validators.required])
   })
+  ngOnInit() {
+  }
 
   toggleEdit() {
     this.isEditing = !this.isEditing;
@@ -22,5 +31,21 @@ export class EstabelecimentoDetailComponent {
 
   cancelEdit() {
     this.isEditing = false;
+  }
+
+  submit() {
+
+    if (this.form.valid) {
+    var valoresDoFormulario = this.form.value;
+
+    console.log(
+      valoresDoFormulario.nomeFantasia + "\n" + valoresDoFormulario.razaoSocial + "\n" + valoresDoFormulario.cnpj + "\n"+
+      valoresDoFormulario.email + "\n" + valoresDoFormulario.telefone + "\n" + valoresDoFormulario.categoriaNegocio + "\n"+
+      valoresDoFormulario.dataCriacao + "\n" + valoresDoFormulario.nomeComprovante
+    );
+    }
+
+
+
   }
 }
